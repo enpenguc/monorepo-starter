fd1f5b4172bf86ecb0de7dbd0e8722d239a561c8
enpeng <enpenguc@163.com>
2021-06-15 14:51:55 +0800

    upgrade rollup

--- packages/callback/src/index.ts
+++ packages/callback/src/index.ts
@@ -1,9 +1,9 @@
-import { isNumber } from "@dec/utils";
+import { isNumber } from '@dec/utils';
 // import { isNumber } from '../../utils';
 
 console.log(typeof isNumber);
 
-// export const checkIsValid = (): boolean => {
-//   const data = '12313';
-//   return isNumber(data);
-// };
+export const checkIsValid = (): boolean => {
+  const data = '12313';
+  return isNumber(data);
+};
